59a44cbc04b4ea7735ce3a458fb9ee81e468ab0d
Botifywm <76607519+Botifywm@users.noreply.github.com>
2024-08-11 14:37:14 +0800

    Change the font to rem instead of "px" to ensure it is dynamic
    Reorganised the script.js to make it neater as well as adding comments for descriptive purposes

--- script.js
+++ script.js
@@ -34,20 +34,15 @@ Book.prototype.toggleRead = function() {
 }
 
 // Display library
-
-// const myLibrary = [{
-//     title: 'The hobbit',
-//     author: 'RKK',
-//     pages: '295',
-//     status: 'Not read'
-// }];
-
 const myLibrary = [];
 
 let book1 = new Book('The hobbit', 'RKK', '295', 'Not read');
 myLibrary.push(book1);
 
 function displayLibrary() {
+    // before looping, we want to empty the table body so it does not 
+    // keep the rows from the previous execution of this function - which will
+    // create duplicates
     const allrows = document.querySelectorAll('tbody tr');
     allrows.forEach((rows) => {
         rows.remove();
@@ -59,9 +54,11 @@ function displayLibrary() {
         num.textContent = index;
         row.appendChild(num);
 
+        // iterating over the object to make it dynamic instead of hardcoding
         Object.values(book).forEach((val, index) => {
             const content = document.createElement('td');
             content.textContent = val;
+            // index is hardcoded here - have to find a way to let it identify the object keys in the future
             if (index === 3) {
                 const btn = document.createElement('button');
                 btn.textContent = "Change Status"
@@ -76,6 +73,7 @@ function displayLibrary() {
 
             row.appendChild(content);
         })
+        // creating a remove button that is outside of the object
         const removeBook = document.createElement('td')
         const removeSvg = '<svg fill="#000000" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>remove</title> <path d="M11.188 4.781c6.188 0 11.219 5.031 11.219 11.219s-5.031 11.188-11.219 11.188-11.188-5-11.188-11.188 5-11.219 11.188-11.219zM11.25 17.625l3.563 3.594c0.438 0.438 1.156 0.438 1.594 0 0.406-0.406 0.406-1.125 0-1.563l-3.563-3.594 3.563-3.594c0.406-0.438 0.406-1.156 0-1.563-0.438-0.438-1.156-0.438-1.594 0l-3.563 3.594-3.563-3.594c-0.438-0.438-1.156-0.438-1.594 0-0.406 0.406-0.406 1.125 0 1.563l3.563 3.594-3.563 3.594c-0.406 0.438-0.406 1.156 0 1.563 0.438 0.438 1.156 0.438 1.594 0z"></path> </g></svg>'
         removeBook.innerHTML = removeSvg;
@@ -91,6 +89,7 @@ function displayLibrary() {
 
 displayLibrary();
 
+// Dialog form
 showButton.addEventListener("click", () => {
     dialog.showModal();
   });
